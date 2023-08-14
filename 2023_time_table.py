4fd78141f4b594f9fc15e12deb86672f98f208db
Deepak Dhanawade <121153915+Deepak-git-hub27@users.noreply.github.com>
2023-08-14 13:06:23 +0530

    Update 2023_time_table.py

--- 2023_time_table.py
+++ 2023_time_table.py
@@ -30,5 +30,6 @@ def get_time_table(time_table1):
         if sub=='English':
 
             st.write("Today is english period kindly carry colors")
-    
+
+st.write("Last update 14th Aug'23")
 get_time_table(time_table)
