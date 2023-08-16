86fe6127d3492a5c6b150832871b1bf43cb03c62
Deepak Dhanawade <121153915+Deepak-git-hub27@users.noreply.github.com>
2023-08-16 10:20:12 +0530

    Update 2023_time_table.py

--- 2023_time_table.py
+++ 2023_time_table.py
@@ -8,6 +8,14 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            footer:after {
+	content:'Welcome'; 
+	visibility: visible;
+	display: block;
+	position: relative;
+	#background-color: red;
+	padding: 5px;
+	top: 2px;
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
