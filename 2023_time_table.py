78e306356a9b1eb36e9129f71cccbe2423c21fa6
Deepak Dhanawade <121153915+Deepak-git-hub27@users.noreply.github.com>
2023-08-18 12:25:29 +0530

    Update 2023_time_table.py

--- 2023_time_table.py
+++ 2023_time_table.py
@@ -28,7 +28,9 @@ time_table= {'Monday':['Computer','Hindi','CCA','English','Marathi','EVS','Maths
 
 
 holiday_list=['2023-8-15','2023-8-29','2023-8-30','2023-9-07','2023-9-19','2023-9-28','2023-10-02','2023-10-24','2023-11-27','2024-1-15','2024-1-26','2024-2-19','2024-3-08','2024-3-25','2024-3-29']
-    
+
+
+
 def check_holiday():
     
     for holiday in holiday_list:
@@ -43,7 +45,25 @@ def check_holiday():
             
             get_time_table(time_table)
             break
-    
+
+def activity_check():
+    today=dt.date.today()+dt.timedelta(days=1)
+
+    first_day = datetime.date(dt.date.today().year,dt.date.today().month, 1)
+
+    day_offset = (3 - first_day.weekday()) % 7
+    first_thursday = first_day + datetime.timedelta(days=day_offset)
+    third_thursday = first_thursday + datetime.timedelta(weeks=2)
+
+
+    second_thursday = first_thursday + datetime.timedelta(days=7)
+    fourth_thursday = third_thursday + datetime.timedelta(days=7)
+
+    if today==first_thursday or today==third_thursday:
+        return'Tomorrow is sketing day'
+    elif today==second_thursday or today==fourth_thursday:
+        return'Tomorrow is self-defense activity'
+
 def get_time_table(time_table1):
     
 
@@ -59,7 +79,7 @@ def get_time_table(time_table1):
     if day=='Tuesday':
         st.write("Kindly wear PT uniform.")
     elif day=='Thursday':
-        st.write("Kindly wear PT uniform.")
+        st.write(f"Kindly wear PT uniform & its {activity_check()}")
              
     st.write("Note:-Last update 14th Aug'23") 
 
