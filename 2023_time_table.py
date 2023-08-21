bbc1fd0ff347dfb823a87df8167285ad252c3c49
Deepak Dhanawade <121153915+Deepak-git-hub27@users.noreply.github.com>
2023-08-21 10:52:25 +0530

    Update 2023_time_table.py

--- 2023_time_table.py
+++ 2023_time_table.py
@@ -2,10 +2,6 @@ import datetime as dt
 from datetime import timedelta,datetime
 import streamlit as st
 
-with st.sidebar:
-    add_radio = st.radio(
-        "2nd B Time Table",
-        "Tommorws's Time Table", "Ask me")
 
 today=dt.date.today()+dt.timedelta(days=1)
 
